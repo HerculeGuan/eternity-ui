7841b0cc07fcfa5bc122e0d793f09e5f993eb6bc
holmes <15221038479@163.com>
2021-01-25 22:28:38 +0800

    引入parcel 并添加button组件

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import Button from "./components/button.vue"
+
+Vue.component('g-button', Button)
+
+new Vue({
+    el:"#app"
+})
---
+++ src/components/button.vue
@@ -0,0 +1,69 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@keyframes circle {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  cursor: pointer;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
+    }
+    > .content {
+      order: 1;
+    }
+  }
+  .loading {
+    animation: circle 1s infinite linear;
+  }
+}
+</style>
+
