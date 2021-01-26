29d84c8defecea16dbcfe66a31d6ccd283566fda
holmes <15221038479@163.com>
2021-01-26 22:34:27 +0800

    commit buttom test

--- src/app.js
+++ src/app.js
@@ -30,4 +30,70 @@ const expect = chai.expect
     let useElement = button.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#i-setting')
+    button.$el.remove();
+    button.$destroy();
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    button.$el.remove();
+    button.$destroy();
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    button.$el.remove();
+    button.$destroy();
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right',
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove();
+    button.$destroy();
+}
+{
+    const Constructor = Vue.extend(Button)
+    const gButton = new Constructor({
+        propsData: {
+            icon: 'setting',
+        }
+    })
+    gButton.$mount()
+    gButton.$on('click',()=>{
+        console.log('2')
+    })
+    let button = gButton.$el
+    button.click()
+    gButton.$el.remove();
+    gButton.$destroy();
 }
