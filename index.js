716fa85357ac2c79bfc007059015a289f55d658b
holmes <15221038479@163.com>
2021-01-27 20:04:01 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import Button from './src/components/Button/button.vue'
+import ButtonGroup from './src/components/Button/button-group.vue'
+import Icon from './src/components/Icon/icon.vue'
+
+export {Button,ButtonGroup,Icon}
