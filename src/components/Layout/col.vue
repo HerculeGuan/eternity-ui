b40c1389515c210143701b667df0e391480bec11
holmes <15221038479@163.com>
2021-02-01 21:39:43 +0800

    完成响应式

--- src/components/Layout/col.vue
+++ src/components/Layout/col.vue
@@ -24,15 +24,15 @@ export default {
     offset: {
       type: [Number, String],
     },
-    phone: {
-      type: Object,
-      validator,
-    },
     ipad: {
       type: Object,
       validator,
     },
     narrowPc: {
+      type: Object,
+      validator,
+    },
+    pc: {
       type: Object,
       validator,
     },
@@ -48,14 +48,14 @@ export default {
   },
   computed: {
     colClass() {
-      let { span, offset, phone, ipad, narrowPc, widePc } = this;
+      let { span, offset, ipad, narrowPc, pc, widePc } = this;
       return [
         span && `col-${span}`,
         offset && `offset-${offset}`,
-        ...(phone && [`col-phone-${phone.span}`]),
-        ...(ipad && [`col-ipad-${ipad.span}`]),
-        ...(narrowPc && [`col-narrowPc-${narrowPc.span}`]),
-        ...(widePc && [`col-widePc-${widePc.span}`]),
+        ...(ipad ? [`col-ipad-${ipad.span}`] : []),
+        ...(narrowPc ? [`col-narrowPc-${narrowPc.span}`] : []),
+        ...(pc ? [`col-pc-${pc.span}`] : []),
+        ...(widePc ? [`col-widePc-${widePc.span}`] : []),
       ];
     },
     colStyle() {
@@ -80,18 +80,6 @@ export default {
       margin-left: ($n / 24) * 100%;
     }
   }
-  @media (max-width: 576px) {
-    $class-prefix-col: col-phone-;
-    $class-prefix-offset: offset-;
-    @for $n from 1 through 24 {
-      &.#{$class-prefix-col}#{$n} {
-        width: ($n / 24) * 100%;
-      }
-      &.#{$class-prefix-offset}#{$n} {
-        margin-left: ($n / 24) * 100%;
-      }
-    }
-  }
   @media (min-width: 577px) and (max-width: 768px) {
     $class-prefix-col: col-ipad-;
     $class-prefix-offset: offset-;
@@ -106,6 +94,18 @@ export default {
   }
   @media (min-width: 769px) and (max-width: 1200px) {
     $class-prefix-col: col-narrowPc-;
+    $class-prefix-offset: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix-col}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+      &.#{$class-prefix-offset}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1201px) and (max-width: 1920px) {
+    $class-prefix-col: col-pc-;
     $class-prefix-offset: offset-;
     @for $n from 1 through 24 {
       &.#{$class-prefix-col}#{$n} {
