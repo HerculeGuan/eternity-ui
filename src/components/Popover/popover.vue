a5edc96f85182938a8dc44fbac7e9fef78d45013
holmes <15221038479@163.com>
2021-02-22 21:50:59 +0800

    把popover append 到body上

--- src/components/Popover/popover.vue
+++ src/components/Popover/popover.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div
+      ref="contentWrapper"
+      class="content-wrapper"
+      v-if="visible"
+      @click.stop
+    >
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -20,6 +27,11 @@ export default {
       this.visible = !this.visible;
       if (this.visible === true) {
         setTimeout(() => {
+          document.body.appendChild(this.$refs.contentWrapper);
+          let { top, left } = this.$refs.triggerWrapper.getBoundingClientRect();
+          console.log(top, left);
+          this.$refs.contentWrapper.style.left = `${left+ window.scrollX}px`;
+          this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`;
           let eventHandler = () => {
             this.visible = false;
             document.removeEventListener("click", eventHandler);
@@ -29,6 +41,7 @@ export default {
       }
     },
   },
+  mounted() {},
 };
 </script>
 
@@ -37,12 +50,11 @@ export default {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  > .content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  }
+}
+.content-wrapper {
+  position: absolute;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
 }
 </style>
