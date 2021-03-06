9b9645340ed62dc90b554a2420906ab045b611b2
holmes <15221038479@163.com>
2021-03-06 17:03:03 +0800

    修改部分文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 module.exports = {
     base: '/vue-wheel/',
     title: 'Eternity UI',
--- index.js
+++
@@ -1,5 +0,0 @@
-import Button from './src/components/Button/button.vue'
-import ButtonGroup from './src/components/Button/button-group.vue'
-import Icon from './src/components/Icon/icon.vue'
-
-export {Button,ButtonGroup,Icon}
