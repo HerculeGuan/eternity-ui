b8e64eae81843312a31f61637f65685b32bd254b
holmes <15221038479@163.com>
2021-03-08 15:05:28 +0800

    导入省市区数据库

--- docs/.vuepress/components/cascader-demos.vue
+++ docs/.vuepress/components/cascader-demos.vue
@@ -19,60 +19,11 @@
 import Cascader from "../../../src/components/Cascader/cascader";
 import CodeWrap from "../utils/code-wrap";
 import "../style/common.scss";
+import db from "../../../src/components/Cascader/db";
 
-const db = [
-  {
-    name: "河北省",
-    children: [
-      {
-        name: "保定市",
-        children: [{ name: "莲池区" }, { name: "满城区" }, { name: "涞水县" }],
-      },
-      {
-        name: "石家庄市",
-        children: [{ name: "长安区" }, { name: "桥西区" }],
-      },
-      {
-        name: "唐山市",
-        children: [
-          {
-            name: "遵化市",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    name: "江苏省",
-    children: [
-      {
-        name: "南京市",
-        children: [{ name: "玄武区" }, { name: "秦淮区" }, { name: "鼓楼区" }],
-      },
-      {
-        name: "无锡市",
-        children: [{ name: "锡山区" }, { name: "惠山区" }, { name: "滨湖区" }],
-      },
-    ],
-  },
-  {
-    name: "上海市",
-    children: [
-      {
-        name: "杨浦区",
-      },
-      {
-        name: "宝山区",
-      },
-    ],
-  },
-];
-function ajax(name, level) {
-  if (!name && !level) {
-    return db.map((item) => item.name);
-  }
+function ajax(parentId = 0) {
+  return db.filter((item) => item.parent_id == parentId);
 }
-console.log(ajax());
 
 export default {
   components: {
@@ -82,7 +33,66 @@ export default {
   data() {
     return {
       selected: [],
-      options: db,
+      options: ajax(),
+      // options: [
+      //   {
+      //     name: "河北省",
+      //     children: [
+      //       {
+      //         name: "保定市",
+      //         children: [
+      //           { name: "莲池区" },
+      //           { name: "满城区" },
+      //           { name: "涞水县" },
+      //         ],
+      //       },
+      //       {
+      //         name: "石家庄市",
+      //         children: [{ name: "长安区" }, { name: "桥西区" }],
+      //       },
+      //       {
+      //         name: "唐山市",
+      //         children: [
+      //           {
+      //             name: "遵化市",
+      //           },
+      //         ],
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     name: "江苏省",
+      //     children: [
+      //       {
+      //         name: "南京市",
+      //         children: [
+      //           { name: "玄武区" },
+      //           { name: "秦淮区" },
+      //           { name: "鼓楼区" },
+      //         ],
+      //       },
+      //       {
+      //         name: "无锡市",
+      //         children: [
+      //           { name: "锡山区" },
+      //           { name: "惠山区" },
+      //           { name: "滨湖区" },
+      //         ],
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     name: "上海市",
+      //     children: [
+      //       {
+      //         name: "杨浦区",
+      //       },
+      //       {
+      //         name: "宝山区",
+      //       },
+      //     ],
+      //   },
+      // ],
       content1: `
     null
     `,
