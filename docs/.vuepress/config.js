b1b43d395f5d33e78bebb7230ab3a0fc4b6f09f4
holmes <15221038479@163.com>
2021-03-13 13:12:58 +0800

    配置 vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,39 +1,37 @@
-const path = require('path')
+const path = require("path");
 module.exports = {
-    base: '/vue-wheel/',
-    title: 'Eternity UI',
-    description: 'Vue UI 组件库',
-    themeConfig: {
-        nav: [
-            { text: '首页', link: '/' },
-            { text: 'Github', link: 'https://google.com' },
+  base: "/vue-wheel/",
+  title: "Eternity UI",
+  description: "Vue UI 组件库",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "Github", link: "https://github.com/HerculeGuan/vue-wheel" },
+    ],
+    sidebar: [
+      {
+        title: "开发指南",
+        children: ["/install/", "/get-started/"],
+        collapsable: false,
+      },
+      {
+        title: "组件",
+        children: [
+          "/components/icon",
+          "/components/button",
+          "/components/input",
+          "/components/layout",
+          "/components/container",
+          "/components/toast",
+          // '/components/popover',
+          "/components/tabs",
+          "/components/collapse",
+          "/components/cascader",
         ],
-        sidebar: [
-            {
-                title: '开发指南',
-                children: [
-                    '/install/',
-                    '/get-started/',
-                ],
-                collapsable: false,
-            },
-            {
-                title: '组件',
-                children: [
-                    '/components/icon',
-                    '/components/button',
-                    '/components/input',
-                    '/components/layout',
-                    '/components/container',
-                    '/components/toast',
-                    '/components/popover',
-                    '/components/tabs',
-                    '/components/collapse',
-                    '/components/cascader',
-                ],
-                collapsable: false,
-            }
-        ],
-
-    }
-}
+        collapsable: false,
+      },
+    ],
+    smoothScroll: true,
+    lastUpdated: "最后更新时间", // string | boolean
+  },
+};
